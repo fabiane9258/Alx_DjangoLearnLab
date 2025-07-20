c78cf704f7889d6bf649ada9019626c623f54440
fabiane9258 <fabiane9258@gmail.com>
2025-07-20 23:06:54 +0300

    Added templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# View to display details of a single library
+# View to display details of a specific library
 def library_detail(request, library_id):
     library = get_object_or_404(Library, id=library_id)
     return render(request, 'relationship_app/library_detail.html', {'library': library})
