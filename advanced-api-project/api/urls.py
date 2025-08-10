90ffbd089c3419790d81f8989bff2cac886e32e6
fabiane9258 <fabiane9258@gmail.com>
2025-08-10 14:23:33 +0300

    Refactor Book views to include Create, Update, Delete

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,8 +1,17 @@
 # api/urls.py
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import (
+    BookListView,
+    BookDetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create', CreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', UpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', DeleteView.as_view(), name='book-delete'),
 ]
