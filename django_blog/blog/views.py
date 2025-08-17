3bbce6bdce12733b124dfa254cf36bcc558118eb
fabiane9258 <fabiane9258@gmail.com>
2025-08-17 23:36:39 +0300

    added LoginRequiredMixin

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render, redirect
 from django.db.models import Q
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, ProfileForm
 from taggit.models import Tag
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import ProfileForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 @login_required
 def profile_view(request):
-    if request.method == "POST":  #handles POST
+    if request.method == "POST":  # handles POST
         form = ProfileForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()  # save()
@@ -21,17 +21,17 @@ def profile_view(request):
 
     return render(request, "blog/profile.html", {"form": form})
 
+
 def post_update(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             form.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post-detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_form.html', {'form': form})
-
 
 
 class PostListView(ListView):
@@ -40,26 +40,47 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
-class PostCreateView(CreateView):
+
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
     success_url = reverse_lazy('post-list')
 
-class PostUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # assign logged-in user as author
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
     success_url = reverse_lazy('post-list')
 
-class PostDeleteView(DeleteView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
     success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
 
 class PostByTagListView(ListView):
     model = Post
@@ -70,6 +91,7 @@ class PostByTagListView(ListView):
         tag_slug = self.kwargs.get('tag_slug')
         tag = get_object_or_404(Tag, slug=tag_slug)
         return Post.objects.filter(tags=tag)
+
 
 class SearchResultsView(ListView):
     model = Post
