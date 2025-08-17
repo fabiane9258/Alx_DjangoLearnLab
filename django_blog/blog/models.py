69eb6b0b2b11283ffb0f873d4f52b548691ac948
fabiane9258 <fabiane9258@gmail.com>
2025-08-17 23:16:15 +0300

    added published date field

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
-from taggit.managers import TaggableManager
+from taggit.managers import TaggableManager  # for tags
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)  # required field
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(auto_now_add=True)
-    #tags = TaggableManager()  # ✅ tagging#
+    tags = TaggableManager()  # allows tagging
+
+    def publish(self):
+        """Set published_date to now and save"""
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.title
