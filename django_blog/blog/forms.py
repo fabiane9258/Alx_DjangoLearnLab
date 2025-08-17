45aeb5732dbb738afaac0eac5f4d3b3ed9a8fc80
fabiane9258 <fabiane9258@gmail.com>
2025-08-17 23:51:26 +0300

    added comment form inside forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from .models import Post
 from taggit.forms import TagWidget
 
-# ✅ Post Form
+# post Form
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -12,8 +12,22 @@ class PostForm(forms.ModelForm):
             'tags': TagWidget(),
         }
 
-# ✅ User Profile Update Form
+# User Profile Update Form
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
+
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+    # Optional: add validation rules
+    def clean_content(self):
+        content = self.cleaned_data.get("content")
+        if len(content.strip()) < 3:
+            raise forms.ValidationError("Comment must be at least 3 characters long.")
+        return content
