ba92a521b4ef0d3ff94411c266e2b94725494929
fabiane9258 <fabiane9258@gmail.com>
2025-08-24 14:07:50 +0300

    added login and register routes to accounts/urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView, TestUserView
+from .views import RegisterView, LoginView, ProfileView
 
 urlpatterns = [
-    path("register", RegisterView.as_view(), name="register"),
-    path("login", LoginView.as_view(), name="login"),
-    path("profile", ProfileView.as_view(), name="profile"),
-    path("test-user/", TestUserView.as_view(), name="test_user"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
